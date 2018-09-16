e19384d14925634f859f07a2783dd1c2032ed790
Shen <hackersk@qq.com>
2018-09-16 19:10:14 +0800

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,36 @@
+# coding: utf8
+
+from azzert import azzert, ensure, mock, C, D, E
+
+data = {
+    'id': '123',
+    'name': 'tom',
+    'contact': {
+        'type': 'mobile',
+        'value': '10010001000',
+    },
+    'hobbies': ['Swimming', 'movie'],
+}
+
+schema = {
+    'id': (int, r'^\d+$', E(456)),
+    'name': (str, E('jerry')),
+    'age': (D(18), E(28)),
+    'contact': {
+        'type': {'mobile', 'email'},
+        'value': ((True, str, lambda v: 0 < len(v) < 100), E('10020003000')),
+    },
+    'hobbies': ([(True, str, C(lambda s: s.lower()))], None, E(['Running', 'chess'])),
+}
+
+result = azzert(data, schema)
+
+print result
+
+result = ensure(data, schema)
+
+print result
+
+result = mock(schema)
+
+print result
